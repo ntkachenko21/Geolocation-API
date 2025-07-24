87d31ffc154c577722963a2b84bbe6a443367062
Nikita Tkachenko <7heswatgg@gmail.com>
2025-07-24 13:53:07 +0200

    Delete unused variable

--- places/utils.py
+++ places/utils.py
@@ -2,7 +2,7 @@ import os
 import uuid
 
 
-def place_photo_path(instance, filename):
+def place_photo_path(filename):
     ext = filename.split(".")[-1]
     filename = f"{uuid.uuid4()}.{ext}"
     return os.path.join("places", "photos", filename)
